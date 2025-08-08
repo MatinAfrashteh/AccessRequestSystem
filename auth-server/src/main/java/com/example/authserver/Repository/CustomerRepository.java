165b6eb8963786cce57a6dde293d9be6a48b5ff2
MatinAfrashteh <matinafrashteh992@gmail.com>
2025-08-08 12:47:16 +0330

    Add Project App + oauth2 Server

---
+++ auth-server/src/main/java/com/example/authserver/Repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.example.authserver.Repository;
+
+
+import com.example.authserver.Model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
+
---
+++ auth-server/src/main/java/com/example/authserver/SecurityConfig/EazyBankUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.example.authserver.SecurityConfig;
+
+
+import com.example.authserver.Model.Customer;
+import com.example.authserver.Repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        List<GrantedAuthority> authorities = Arrays.stream(customer.getRole().split(","))
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.trim()))
+                .collect(Collectors.toList());
+
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+
+
+}
+
